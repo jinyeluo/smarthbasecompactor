3cafa51cbda56da18414f44cf6c5319f368ce712
Jinye Luo <jinyeluo@gmail.com>
2016-04-29 10:58:46 -0500

    release 1.2

--- scripts/hbasecompact.sh
+++ scripts/hbasecompact.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . ./set_system_env.sh
-hadoop jar smarthbasecompactor-1.0.jar com.luo.HbaseMgr -libjars ${HADOOP_LIB_JARS}  -t 300 -f 0 -c 3
+yarn jar smarthbasecompactor-1.2.jar com.luo.HbaseMgr -libjars ${HADOOP_LIB_JARS}  -t 600 -f 0 -c 3
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . ./set_system_env.sh
-hadoop jar ./smarthbasecompactor-1.1-SNAPSHOT.jar com.luo.merge.HBaseMerge2 -libjars ${HADOOP_LIB_JARS} $1 3G
+yarn jar ./smarthbasecompactor-1.1-SNAPSHOT.jar com.luo.merge.HBaseMerge2 -libjars ${HADOOP_LIB_JARS} $1 3G
